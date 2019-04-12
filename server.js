5eb5410545d0e61e8630e1a87b105b7a1b1b8400
larrygf02 <raulgf_02@hotmail.com>
2019-04-12 00:03:01 -0500

    Servir archivos estaticos

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', function(req, res) {
+app.use( express.static( __dirname + '/public'))
+
+/*app.get('/', function(req, res) {
     let salida = {
         nombre: 'Raul',
         edad: 25,
         url: req.url
     }
     res.send(salida);
-})
+})*/
 
 app.listen(3000, () => {
     console.log('Escuchando puerto 3000');
