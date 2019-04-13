2a9130490d07df4523f40f8eb521e36a57152db9
larrygf02 <raulgf_02@hotmail.com>
2019-04-12 19:53:48 -0500

    Usando parciales en hbs

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ app.use( express.static( __dirname + '/public'))
 //Para usar handlebars
 app.set('view engine', hbs)
 
+hbs.registerPartials( __dirname + '/views/partials');
 app.get('/', function(req, res) {
 
     res.render('home.hbs', {
         name: 'Raul',
         anio: new Date().getFullYear()
     });
+})
+
+app.get('/about', function(req,res) {
+    res.render('about.hbs', {
+        name: 'Raul'
+    })
 })
 
 app.listen(3000, () => {
